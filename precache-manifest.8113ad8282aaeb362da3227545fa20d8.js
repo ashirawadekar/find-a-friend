2fbd7a62afc1d8f3449a422044c717bce4d0f015
Aniket Shirawadekar <ashirawadekar@gmail.com>
2020-10-17 10:31:44 -0400

    Updates

---
+++ precache-manifest.8113ad8282aaeb362da3227545fa20d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9566cd62515b17f98ff3746bbbf588",
+    "url": "/find-a-friend/index.html"
+  },
+  {
+    "revision": "42c9187bf4f2c87791a1",
+    "url": "/find-a-friend/static/css/main.4e447c32.chunk.css"
+  },
+  {
+    "revision": "1de7439a1e7fec74789b",
+    "url": "/find-a-friend/static/js/2.38cf27f7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/find-a-friend/static/js/2.38cf27f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c9187bf4f2c87791a1",
+    "url": "/find-a-friend/static/js/main.325ea4ec.chunk.js"
+  },
+  {
+    "revision": "975150fd9ceca99b1f0a",
+    "url": "/find-a-friend/static/js/runtime-main.5b1cbdf2.js"
+  }
+]);
